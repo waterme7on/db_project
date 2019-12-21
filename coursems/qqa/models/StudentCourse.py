9ce1ac365dc21cbd6209d2167651faccd886f614
Losk-LuoYuanhui <1261603649@qq.com>
2019-12-21 18:42:37 +0800

    解决了migrate中的问题

--- coursems/qqa/models/StudentCourse.py
+++ coursems/qqa/models/StudentCourse.py
@@ -1,25 +1,14 @@
 from django.db import models
-from qqa.models import Student
-#from qqa.models import Teacher
-from qqa.models import Course
+from qqa.models.Student import Student
+from qqa.models.Teacher import Teacher
+from qqa.models.Course import Course
 
-<<<<<<< HEAD
 class StudentCourse(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     semester = models.CharField(max_length=15, verbose_name='学期')
     score = models.IntegerField(verbose_name='成绩', null=True, blank=True)
-=======
-class SC(models.Model):
-
-    # 数据项的定义
-    student_no = models.ForeignKey(Student, on_delete=models.CASCADE)
-    course_no = models.ForeignKey(Course, on_delete=models.CASCADE)
-    #teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-    semester_no = models.CharField(max_length=15, verbose_name='学期')
-    score_no = models.IntegerField(verbose_name='成绩', null=True, blank=True)
->>>>>>> 95f83b6d22834e657b63230eaa83cae0412df421
 
     def __str__(self):
         return str(self.student_no)+"got "+str(self.score_no)+"in"+str(self.course_no)
