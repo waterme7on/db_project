98485c66e0d4bd887c19fad4cc031f271abe48bc
xqy <1241587856@qq.com>
2019-12-21 18:00:01 +0800

    update model

--- coursems/qqa/models/StudentCourse.py
+++ coursems/qqa/models/StudentCourse.py
@@ -4,14 +4,16 @@ from .Teacher import Teacher
 from .Course import Course
 
 class SC(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-    semester = models.CharField(max_length=15, verbose_name='学期')
-    score = models.IntegerField(verbose_name='成绩', null=True, blank=True)
+
+    # 数据项的定义
+    student_no = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course_no = models.ForeignKey(Course, on_delete=models.CASCADE)
+    #teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    semester_no = models.CharField(max_length=15, verbose_name='学期')
+    score_no = models.IntegerField(verbose_name='成绩', null=True, blank=True)
 
     def __str__(self):
-        return self.semester
+        return self.semester_no
 
     class Meta:
         app_label = 'qqa'
