593874296989a950366751247598ee7478b945e8
Losk-LuoYuanhui <1261603649@qq.com>
2019-12-25 18:56:10 +0800

    修改了密码系统的小bug, 新增了系统管理员用户

--- coursems/qqa/models/SchoolAdmin.py
+++ coursems/qqa/models/SchoolAdmin.py
@@ -5,6 +5,7 @@ from qqa.models.Teacher import Teacher
 class SchoolAdmin(models.Model):
     school = models.ForeignKey(School, on_delete=models.CASCADE, unique=True)
     admin = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    password = models.CharField(max_length=100, blank=True)
     
     def __str__(self):
         return str(self.school) + "'s admin is " + str(self.admin)
