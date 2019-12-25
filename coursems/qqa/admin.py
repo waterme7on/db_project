eb9de2bd4e651a0922a12070f000e3419fa6208a
watermelon333 <watermelonlee@protonmail.com>
2019-12-25 17:11:03 +0800

    修正db

--- coursems/qqa/admin.py
+++ coursems/qqa/admin.py
@@ -2,12 +2,21 @@ from django.contrib import admin
 from .models.Course import Course
 from .models.Student import Student
 from .models.Teacher import Teacher
+from .models.School import School
 from .models.TeacherCourse import TeacherCourse
 from .models.StudentCourse import StudentCourse
+from .models.CourseSchool import CourseSchool
+from .models.SchoolAdmin import SchoolAdmin
+from .models.StudentSchool import StudentSchool
+
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Teacher)
 admin.site.register(Course)
+admin.site.register(School)
 admin.site.register(TeacherCourse)
 admin.site.register(StudentCourse)
+admin.site.register(CourseSchool)
+admin.site.register(SchoolAdmin)
+admin.site.register(StudentSchool)
