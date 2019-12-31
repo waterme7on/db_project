d248832a27855584e0770e66ed16d35d2256209e
watermelon333 <watermelonlee@protonmail.com>
2019-12-31 20:20:29 +0800

    Update 数据库中的StudentCourlas

--- coursems/qqa/models/StudentCourlas.py
+++ coursems/qqa/models/StudentCourlas.py
@@ -5,9 +5,9 @@ from qqa.models import Courlas
 class StudentCourlas(models.Model):
     student_no = models.ForeignKey(Student, on_delete=models.CASCADE)
     courlas_no = models.ForeignKey(Courlas, on_delete=models.CASCADE)
-    status = models.IntegerField()
+    # status = models.IntegerField()
     semester = models.CharField(max_length=10)
-    intention = models.IntegerField()
+    # intention = models.IntegerField()
 
     def __str__(self):
         return str(self.student_no) + " select " + str(self.courlas_no)
